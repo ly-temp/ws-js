b74249275eeb1e4befcce0afba92851232aff1b9
ly-temp <100673133+ly-temp@users.noreply.github.com>
2024-08-20 19:53:53 +0800

    parallelism

--- js/main.mjs
+++ js/main.mjs
@@ -65,8 +65,9 @@ async function concat_unread_msg(chat){
         mentioned_me |= ME_REGEX.test(msg.body)
         msg_strg += '\n' + msg.body
     }
-    return {mentioned_me, msg_strg}
+    return {chat, mentioned_me, msg_strg}
 }
+/**
 async function response_chat(chat){
     const {mentioned_me, msg_strg} = await concat_unread_msg(chat)
 
@@ -103,10 +104,69 @@ async function response_chat(chat){
         await chat.sendMessage("[System]:\nemail-forwarded 已轉達電郵"+"\n[laiyuan Bot]:\n"+ai_json.response)
     }
 }
+*/
 
 function bot(){
     check_session_exist()
 
+    client.on('ready', async()=>{
+        console.log('Client ready')
+        const unread_chats = (await client.getChats()).filter(e=>e.unreadCount>0)
+        const chats_details = unread_chats.map(async e=>await concat_unread_msg(chat))
+        .map(async ({chat, mentioned_me, msg_strg})=>{
+            const in_whitelist = chat.name.charAt(0) === '~';
+            const require_res = (!chat.isGroup && (in_whitelist || mentioned_me) )
+                || (chat.isGroup && WHITELIST_GROUP.includes(chat.id._serialized) && mentioned_me)
+        
+            console.log(`res[${require_res}]: `+JSON.stringify({
+                name: chat.name,
+                id: chat.id._serialized,
+                count: chat.unreadCount,
+                mentioned_me: mentioned_me,
+                in_whitelist: in_whitelist
+            }))
+
+            return {
+                chat,
+                body:{
+                    id: chat.id._serialized,
+                    name: chat.name,
+                    text: msg_strg,
+                    require_res: require_res
+                }
+            }
+        })
+
+        await Promise.all(chats_details)
+
+        const ai_json = await (await fetch(process.env.AI_URL,{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json',
+                Authorization: process.env.INCOME_API_KEY
+            },
+            body:JSON.stringify({
+                title:'Whatsapp',
+                body:{
+                    chats: chats_details.map(e=>e.body)
+                }
+            })
+        })).json()
+
+        await Promise.all(ai_json.response.map(async({id, ai_output})=>{
+            const chats_detail = chats_details.find(e=>e.body.id === id)
+            const chat = chats_detail.chat
+            const {require_res} = chats_detail.body
+            if(require_res)
+                await chat.sendMessage("[System]:\nemail-forwarded 已轉達電郵"+"\n[laiyuan Bot]:\n"+ai_output)
+            await chat.sendSeen()
+        }))
+
+        setTimeout(()=>{
+            client.destroy()
+        }, 15 *1000)
+    })
+    /**
     client.on('ready', async()=>{
         console.log('Client ready')
         const chats = await client.getChats()
@@ -122,7 +182,7 @@ function bot(){
             client.destroy()
         }, 15 *1000)
     })
-
+    */
     /**
     client.on('message', async message => {
         client.sendMessage(message.from, '[Test] Laiyuan-Bot has received message!')
