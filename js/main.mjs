e47fbeb97df619a05603dfd80f5cfb333c97562f
ly-temp <100673133+ly-temp@users.noreply.github.com>
2024-08-21 01:41:33 +0000

    1

--- js/main.mjs
+++ js/main.mjs
@@ -69,64 +69,67 @@ async function concat_unread_msg(chat){
     return {mentioned_me, msg_strg}
 }
 
+async function handle_unread_chats(unread_chats) {
+    const chats_details = await Promise.all(
+        unread_chats.map(async chat=>{
+            const {mentioned_me, msg_strg} = await concat_unread_msg(chat)
+            const in_whitelist = chat.name.charAt(0) === '~';
+            const require_res = (!chat.isGroup && (in_whitelist || mentioned_me) )
+                || (chat.isGroup && WHITELIST_GROUP.includes(chat.id._serialized) && mentioned_me)
+        
+            console.log(`res[${require_res}]: `+JSON.stringify({
+                name: chat.name,
+                id: chat.id._serialized,
+                count: chat.unreadCount,
+                mentioned_me: mentioned_me,
+                in_whitelist: in_whitelist
+            }))
+
+            return {
+                chat,
+                body:{
+                    id: chat.id._serialized,
+                    name: chat.name,
+                    text: msg_strg,
+                    require_res
+                }
+            }
+        })
+    )
+    
+    const ai_json = await (await fetch(WEBHOOK_URL,{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            Authorization: WEBHOOK_KEY
+        },
+        body:JSON.stringify({
+            title:'Whatsapp',
+            body:{
+                chats: chats_details.map(e=>e.body)
+            }
+        })
+    })).json()
+
+    //console.log(ai_json)
+
+    await Promise.all(ai_json.response.map(async({id, ai_output})=>{
+        const chats_detail = chats_details.find(e=>e.body.id === id)
+        const chat = chats_detail.chat
+        const {require_res} = chats_detail.body
+        if(require_res)
+            await chat.sendMessage("[System]:\nemail-forwarded 已轉達電郵"+"\n[laiyuan Bot]:\n"+ai_output)
+        await chat.sendSeen()
+    }))
+}
 function bot(){
     check_session_exist()
 
     client.on('ready', async()=>{
         console.log('Client ready')
         const unread_chats = (await client.getChats()).filter(chat=>chat.unreadCount>0)
-        const chats_details = await Promise.all(
-            unread_chats.map(async chat=>{
-                const {mentioned_me, msg_strg} = await concat_unread_msg(chat)
-                const in_whitelist = chat.name.charAt(0) === '~';
-                const require_res = (!chat.isGroup && (in_whitelist || mentioned_me) )
-                    || (chat.isGroup && WHITELIST_GROUP.includes(chat.id._serialized) && mentioned_me)
-            
-                console.log(`res[${require_res}]: `+JSON.stringify({
-                    name: chat.name,
-                    id: chat.id._serialized,
-                    count: chat.unreadCount,
-                    mentioned_me: mentioned_me,
-                    in_whitelist: in_whitelist
-                }))
-
-                return {
-                    chat,
-                    body:{
-                        id: chat.id._serialized,
-                        name: chat.name,
-                        text: msg_strg,
-                        require_res
-                    }
-                }
-            })
-        )
-        
-        const ai_json = await (await fetch(WEBHOOK_URL,{
-            method:'POST',
-            headers:{
-                'Content-Type': 'application/json',
-                Authorization: WEBHOOK_KEY
-            },
-            body:JSON.stringify({
-                title:'Whatsapp',
-                body:{
-                    chats: chats_details.map(e=>e.body)
-                }
-            })
-        })).json()
-
-        //console.log(ai_json)
-
-        await Promise.all(ai_json.response.map(async({id, ai_output})=>{
-            const chats_detail = chats_details.find(e=>e.body.id === id)
-            const chat = chats_detail.chat
-            const {require_res} = chats_detail.body
-            if(require_res)
-                await chat.sendMessage("[System]:\nemail-forwarded 已轉達電郵"+"\n[laiyuan Bot]:\n"+ai_output)
-            await chat.sendSeen()
-        }))
-
+        if(unread_chats.length)
+            await handle_unread_chats(unread_chats)
         setTimeout(()=>{
             client.destroy()
         }, 15 *1000)
